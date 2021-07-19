9edd739b12025c60ce749e02733f86380e842ca5
Curtiss Grymala <>
2021-07-19 11:47:27 -0400

    Initial restructure of files for complete rewrite

--- wp-boarddocs-xml.php
+++ wp-boarddocs-xml.php
@@ -7,14 +7,41 @@ Description: Allows you to import and display the contents of a BoardDocs XML fe
 Author: Curtiss Grymala
 Author URI: http://ten-321.com/
 License: GPL2
+Text Domain: wp-boarddocs-xml
+Domain Path: /lang/
 */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	die( 'You do not have permission to access this file directly.' );
+namespace {
+	spl_autoload_register( function ( $class_name ) {
+		if ( ! stristr( $class_name, 'UMW\BoardDocs\\' ) && ! stristr( $class_name, 'UMW\Common\\' ) ) {
+			return;
+		}
+
+		$class_name = str_replace( 'UMW\BoardDocs\\', 'UMW\BoardDocs\/classes/', $class_name );
+
+		$filename = trailingslashit( plugin_dir_path( __FILE__ ) ) . 'lib/' . strtolower( str_replace( array(
+				'\\',
+				'_'
+			), array( '/', '-' ), $class_name ) ) . '.php';
+
+		if ( defined( 'WP_DEBUG' ) && WP_DEBUG ) {
+			error_log( 'Attempting to find class definition at: ' . $filename );
+		}
+
+		if ( ! file_exists( $filename ) ) {
+			return;
+		}
+
+		include $filename;
+	} );
 }
 
-if ( ! class_exists( 'WP_BoardDocs_XML' ) ) {
-	require_once( plugin_dir_path( __FILE__ ) . '/classes/class-wp-boarddocs-xml.php' );
-	global $wp_boarddocs_xml_obj;
-	$wp_boarddocs_xml_obj = WP_BoardDocs_XML::instance();
+namespace UMW\BoardDocs {
+	Plugin::instance();
+
+	add_action( 'plugins_loaded', 'UMW\BoardDocs\load_plugin_textdomain' );
+
+	function load_plugin_textdomain() {
+		\load_plugin_textdomain( 'wp-boarddocs-xml', false, dirname( plugin_basename( __FILE__ ) ) . '/lang/' );
+	}
 }
